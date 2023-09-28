b396f5e0ec7da6d046ab8c50b94c8ede841d31da
KobeSergio <ckasergio@gmail.com>
2023-09-28 09:31:37 +0800

    Quick fix: RO for IMAT task

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -402,8 +402,8 @@ export default function Dashboard() {
                         </h3>
                         <h3 className=" col-span-2 font-monts font-semibold text-sm text-center text-darkerGray px-4">
                           {
-                            row.inspection_task.includes("NIM")
-                              ? "For NIM and VS"
+                            row.inspection_task.includes("IMAT")
+                              ? "For IMAT and VS"
                               : row.inspection_task
                                   .replace(/<[^>]+>/g, "")
                                   .trim() //Removes <>
